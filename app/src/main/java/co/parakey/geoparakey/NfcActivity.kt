06be86305cae8f89f68fee2d2e3d19a04110a3ca
Humberto Linero <82034106+humbertoParakey@users.noreply.github.com>
2021-06-24 12:46:56 +0200

    refactor: rename activity to reflect main project

--- app/src/main/java/co/parakey/geoparakey/NfcActivity.kt
+++ app/src/main/java/co/parakey/geoparakey/NfcActivity.kt
@@ -3,9 +3,9 @@ package co.parakey.geoparakey
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class UnlockActivity : AppCompatActivity() {
+class NfcActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_unlock)
+        setContentView(R.layout.activity_nfc)
     }
 }
